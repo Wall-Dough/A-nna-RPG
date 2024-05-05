b7c72c5c87351850aa893be52b88e799e6a8a3d3
Wall-Dough <awalden0808@gmail.com>
2024-05-05 12:19:29 -0700

    Initial commit

---
+++ Anna.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_direction = Input.get_axis("game_left", "game_right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var y_direction = Input.get_axis("game_up", "game_down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
